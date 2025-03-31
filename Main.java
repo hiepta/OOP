69638d71d2d17858c26e7003c97e7b4850e7d99b
hiepta <trananhhiep0608@gmail.com>
2025-03-31 23:31:39 +0700

    update

--- Main.java
+++ Main.java
@@ -2,34 +2,52 @@ import java.util.Scanner;
 
 class Product{
     private double price;
-    private int quanlity;
+    private double quantity;
 
-    public Product(double price, int quanlity){
+    public Product(double price, double quantity){
         this.price = price;
-        this.quanlity = quanlity;
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQuantity(double quantity){
+        this.quantity = quantity;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    public double getQuantity(){
+        return quantity;
     }
 
     public double totalPrice(){
-         return price * quanlity;
+         return price * quantity;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập giá sản phẩm: ");
-        double price = scanner.nextDouble();
-        int quanlity;
-        do{
-            System.out.print("Nhập số lượng sản phẩm:");
-            quanlity = scanner.nextInt();
-            if(quanlity <=0){
-                System.out.println("Nhập số lượng phải lớn hơn 0");
-            }
-        }while (quanlity<=0);
 
-        Product product = new Product(price, quanlity);
-        System.out.print("Giá của sản phẩm là: " + product.totalPrice());
+        System.out.print("The price of the product is: ");
+        double price = scanner.nextDouble();
+
+        double quantity;
+        // Kiểm tra điều kiện của quantity;
+        do{
+            System.out.print("The quantity of the product is: ");
+            quantity = scanner.nextDouble();
+            if(quantity <=0){
+                System.out.println("Quantity must be greater than 0");
+            }
+        }while (quantity<=0);
+        // Hàm khởi tạo product
+        Product product = new Product(price, quantity);
+        System.out.print("Total: " + product.totalPrice() + " VND");
         scanner.close();
     }
 }
